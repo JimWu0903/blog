7eb854239a81151e449303deb3de7c1235c72ed2
吳孟哲 <jim.wu@promeritage.com.tw>
2024-07-12 16:25:53 +0800

    feat: sensitive information use env variable

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from functools import wraps
 from datetime import datetime
 
 from forms import PostForm, RegisterForm, LoginForm, CommentForm
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -31,7 +32,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
 Bootstrap5(app)
 ckeditor = CKEditor(app)
 
@@ -41,7 +42,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -332,4 +333,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5004)
