e6bd0ac026c2f9c1cf2a5b7a6c0d3f3e312099e9
吳孟哲 <jim.wu@promeritage.com.tw>
2024-07-14 10:39:26 +0800

    feat: blog 實作欄位限制及 validator 機制

--- forms.py
+++ forms.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, HiddenField
 from flask_ckeditor import CKEditorField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, Length, ValidationError
+
+
+def length_check(max_length):
+    def _length(form, field):
+        if len(field.data) > max_length:
+            raise ValidationError(f'The input exceeds the maximum length of {max_length} characters.')
+
+    return _length
 
 
 class LoginForm(FlaskForm):
@@ -20,8 +28,10 @@ class RegisterForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField(label='Post Title', validators=[DataRequired()])
     subtitle = StringField(label='Subtitle', validators=[DataRequired()])
-    # author = StringField(label='Author', validators=[DataRequired()])
-    img_url = StringField(label='Background Img', validators=[DataRequired()])
+    img_url = StringField(label='Background Img', validators=[
+        DataRequired(),
+        length_check(250),
+        URL(message='Invalid URL format.')])
     body = CKEditorField(label='Main Content', validators=[DataRequired()])
     submit = SubmitField(label="create Post")
 
